59674ba86c3b066c464bb8d4270b53ca7a8b0511
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-03-05 20:04:13 +0000

    Update test.py

--- vault/workbench/test.py
+++ vault/workbench/test.py
@@ -2,8 +2,10 @@ import os
 
 def oleobj_parser(filename):
     ole = os.system(f"oleobj {filename}")
+    return(ole)
 
 if __name__ == "__main__":
-    file_path = "C:\\Users\\dread\\Code\\vault1337\\vault\\samples\\bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"  # Replace with the path to your OLE file
+    # file_path = "C:\\Users\\dread\\Code\\vault1337\\vault\\samples\\bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"  # Replace with the path to your OLE file
+    file_path = "/var/www/Vault1337/vault/samples/bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"
     result = oleobj_parser(file_path)
-    print(result)
+    # print(result)
