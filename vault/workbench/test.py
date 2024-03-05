3ffa59cd5053f9492e43ff6f12d3affc2626dbcb
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-03-05 20:24:38 +0000

    ole

--- vault/workbench/test.py
+++ vault/workbench/test.py
@@ -1,20 +1,13 @@
 import os
 
 def oleobj_parser(filename):
-    ole = os.system(f"oleobj {filename}")
-<<<<<<< HEAD
-    return(ole)
+    # Run the command and capture the standard output
+    result = os.system(f"oleobj {filename}")
+    
+    return result
 
 if __name__ == "__main__":
-    # file_path = "C:\\Users\\dread\\Code\\vault1337\\vault\\samples\\bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"  # Replace with the path to your OLE file
-    file_path = "/var/www/Vault1337/vault/samples/bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"
+    file_path = "C:\\Users\\dread\\Code\\vault1337\\vault\\samples\\bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"  # Replace with the path to your OLE file
+    # file_path = "/var/www/Vault1337/vault/samples/bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"
     result = oleobj_parser(file_path)
     # print(result)
-=======
-    return ole
-def run(file_path):
-    return oleobj_parser(file_path)
-if __name__ == "__main__":
-    file_path = "C:\\Users\\dread\\Code\\vault1337\\vault\\samples\\bd8c9ae3215333881f06b32aa9f8ffcadd9dab082cd0be8f95d25de59d467084"  # Replace with the path to your OLE file
-    run(file_path)
->>>>>>> be6c3f23721e5c1515f2c9588abedf8a287f7e33
