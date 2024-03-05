18bef4dce52c3e850beb3c95bb865a18f6ba5580
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-03-05 19:08:42 +0000

    Update oletools.py

--- vault/workbench/oletools.py
+++ vault/workbench/oletools.py
@@ -61,8 +61,7 @@ def olemeta_parser(filename):
     
 def oleobj_parser(filename):
     try:
-        with open(filename, 'rb') as file:
-            ole = OleObject.parse(file.read())
+        ole = OleObject.parse(filename)
         return ole
     except Exception as e:
         return f"Error: {str(e)}"
