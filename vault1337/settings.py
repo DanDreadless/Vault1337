0b7450f59d75c5b6c88879b4535488413c2c2d35
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-05-26 14:33:22 +0100

    Update settings.py

--- vault1337/settings.py
+++ vault1337/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.174.128','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.68.126']
 
 # Application definition
 
