c5ef00257cee1ab7423a595888dce34a1dae829b
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-07-06 13:12:32 +0100

    Update runyara.py

--- vault/workbench/runyara.py
+++ vault/workbench/runyara.py
@@ -1,7 +1,23 @@
+import os
 import yara
 
 def run_yara(file_path):
     rules_path = 'vault/yara-rules/'
-    rules = yara.compile(rules_path)
-    matches = rules.match(file_path)
-    return matches
+
+    # Create an empty dictionary to store rules
+    rules_dict = {}
+
+    # Loop through all files in the rules_path
+    for root, dirs, files in os.walk(rules_path):
+        for file in files:
+            if file.endswith('.yar'):
+                file_path = os.path.join(root, file)
+                rules_dict[file] = file_path
+
+    # Compile the rules from the dictionary
+    if rules_dict:
+        compiled_rules = yara.compile(filepaths=rules_dict)
+        matches = compiled_rules.match(file_path)
+        return matches
+    else:
+        return "No YARA rules found in the specified directory."
