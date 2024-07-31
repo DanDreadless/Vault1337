c3cd2bb9249683695d13743f94af06ea5a9e3ae2
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-07-31 11:44:18 +0100

    Update install.sh
    Signed-off-by: Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
-# Update and upgrade system packages
-sudo apt update && sudo apt upgrade -y
-
-# Install necessary packages
-sudo apt install exiftool libmpc-dev git-all -y
-
-# Prompt user for installation directory
-read -p "Enter the directory where you want to clone Vault1337: " install_dir
-cd "$install_dir" || { echo "Directory not found. Exiting."; exit 1; }
-
-# Clone the repository
-git clone https://github.com/DanDreadless/Vault1337/
-cd Vault1337/ || { echo "Cloning failed. Exiting."; exit 1; }
-
-# Change ownership of the repository
-read -p "Enter your Linux username: " username
-sudo chown -R "$username":"$username" .
+# Create necessary directories
+mkdir -p vault/samples
+mkdir -p vault/yara-rules
 
 # Set up Python virtual environment
 python3 -m venv env
 source env/bin/activate
 
-# Install required Python packages
-pip install -r requirements.txt
+# Install required Python packages with error handling
+if pip install -r requirements.txt; then
+    echo "Python packages installed successfully."
+else
+    echo "Failed to install Python packages. Please check the requirements.txt file."
+    exit 1
+fi
+
+# Apply database migrations
+if python3 manage.py makemigrations && python3 manage.py migrate; then
+    echo "Database migrations applied successfully."
+else
+    echo "Failed to apply database migrations."
+    exit 1
+fi
+
+# Prompt user to create a superuser
+if python3 manage.py createsuperuser; then
+    echo "Superuser created successfully."
+else
+    echo "Failed to create superuser."
+    exit 1
+fi
 
 # Generate and display Django secret key
 secret_key=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
 echo "Your Django secret key is: $secret_key"
 echo "Please add this key to your .env file."
-
-# Apply database migrations
-python3 manage.py makemigrations
-python3 manage.py migrate
-
-# Prompt user to create a superuser
-python3 manage.py createsuperuser
-
-# Create necessary directories
-mkdir -p vault/samples
-mkdir -p vault/yara-rules
-
-# Run the Django development server
-python3 manage.py runserver
