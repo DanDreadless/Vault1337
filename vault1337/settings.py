964ebfbf213452ee8eee593f97383d23b52a0bd9
Dan Dreadless <43904130+DanDreadless@users.noreply.github.com>
2024-09-03 16:25:34 +0100

    Update settings.py

--- vault1337/settings.py
+++ vault1337/settings.py
@@ -128,7 +128,7 @@ DATETIME_FORMAT="%Y-%m-%d%H:%M:%S"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'vault/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
